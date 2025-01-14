930afe2d205cf113913e79b07001c9083948d777
akash3444 <akashmoradiya3444@gmail.com>
2025-01-14 22:08:02 +0530

    feat: remove header breadcrumbs

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,6 @@
 import { createClient } from "@/utils/supabase/server";
 import LoginButton from "./auth/login-button";
 import LogoutButton from "./auth/logout-button";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "./ui/breadcrumb";
-import { Separator } from "./ui/separator";
 import { SidebarTrigger } from "./ui/sidebar";
 
 const Header = async () => {
@@ -20,20 +11,6 @@ const Header = async () => {
     <header className="flex h-16 shrink-0 items-center justify-between gap-2 border-b px-4 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
       <div className="flex items-center gap-2">
         <SidebarTrigger className="-ml-1" />
-        <Separator orientation="vertical" className="mr-2 h-4" />
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem className="hidden md:block">
-              <BreadcrumbLink href="#">
-                Building Your Application
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator className="hidden md:block" />
-            <BreadcrumbItem>
-              <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
       </div>
 
       {data?.user && !error ? <LogoutButton /> : <LoginButton />}
