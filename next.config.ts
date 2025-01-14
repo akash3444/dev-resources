66a0e20ef0e58debccb4ed53ce440f3d053f15d1
akash3444 <akashmoradiya3444@gmail.com>
2025-01-14 15:05:06 +0530

    feat: remove sanity

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
         hostname: "www.google.com",
       },
       {
-        hostname: "cdn.sanity.io",
-      },
-      {
         hostname: "xwiqrnwysxhuuwdfigqc.supabase.co",
       },
     ],
--- src/queries/category.ts
+++
@@ -1,4 +0,0 @@
-export const CATEGORIES_QUERY = `*[_type == "category" && !(_id in path("drafts.**"))]{
-  _id,
-  title,
-}`;
